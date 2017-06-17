fe0f8fd6f8d7d240cc149d6bbdf42224a7412bf4
Antonio Benitez <antbenhid@uma.es>
2017-06-17 13:56:27 +0200

    Updated repository

--- package.js
+++ package.js
@@ -1,10 +1,9 @@
-{
+﻿{
   "name": "GraphQL Server",
   "version": "1.0.0",
   "description": "GraphQL Server Example",
-  "main": "index.js",
   "repository": {
-    "url": "https://github.com/yarnpkg/example-yarn-package",
+    "url": "https://github.com/benhid/graphql-js",
     "type": "git"
   },
   "author": "Antonio Benítez",
